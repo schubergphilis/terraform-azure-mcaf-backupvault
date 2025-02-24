a415d75b026744447418440962ca3e36570ac6b5
Gillian Stravers <gillianstravers@icloud.com>
2025-02-24 14:36:00 +0100

    fix output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,3 @@
-output "backup_vault_identity" {
+output "system_assigned_identity_id" {
   value = azurerm_data_protection_backup_vault.this.identity[0].principal_id
-  description = "System Assigned Identity Principal Id of the Backup Vault"
 }
